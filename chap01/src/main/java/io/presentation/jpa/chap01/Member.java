c118eab87242f3387d8b6538c0cf04c876e75be9
yoonminhyuk <ymh92730@makeus.com>
2018-07-23 15:09:02 +0900

    [ Add ]
    1. pom.xml
    	- hibernate-entitymanger, mysql-connector, junit 의존성 추가
    
    2. Member
    	- member 테이블에 mapping 하기위한 class
    
    3. persistence.xml
    	- JPA는 persistence.xml을 사용해서 필요한 설정 정보를 관리한다. 그래서 생 to the 성
    
    [ Test ]
    
    1. MemberTest
    	- Member 에 대한 CRUD Test

---
+++ chap01/src/main/java/io/presentation/jpa/chap01/Member.java
@@ -0,0 +1,84 @@
+package io.presentation.jpa.chap01;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @Author Minhyuk Yoon
+ * 2018. 07. 23 Created By Minhyuk Yoon
+ **/
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    /**
+     * JPA에서 Default 생성지는 필수 (public or protected)
+     **/
+    protected Member() {
+    }
+
+    public Member(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void changeName(String name) {
+        if (name.trim().isEmpty() || name == null) {
+            return;
+        }
+        this.name = name;
+    }
+
+    public void changeAge(int age) {
+        if (age <= 0) {
+            return;
+        }
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
---
+++ chap01/src/test/java/io/presentation/jpa/chap01/test/MemberTest.java
@@ -0,0 +1,127 @@
+package io.presentation.jpa.chap01.test;
+
+import io.presentation.jpa.chap01.Member;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author Minhyuk Yoon
+ * 2018. 07. 23 Created By Minhyuk Yoon
+ **/
+public class MemberTest {
+
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+    private EntityTransaction transaction;
+
+    @Before
+    public void doBeforeTask() {
+        this.entityManagerFactory = Persistence.createEntityManagerFactory("chap01");
+        this.entityManager = entityManagerFactory.createEntityManager();
+        this.transaction = entityManager.getTransaction();
+    }
+
+    @Test
+    public void testPersist() {
+
+        wrapTryCatch(() -> {
+
+            //Given
+            String id = "id";
+            String name = "name";
+            int age = 10;
+
+            Member member = new Member(id, name, age);
+
+            //When
+            entityManager.persist(member);  // INSERT
+
+            Member findMember = entityManager.find(Member.class, member.getId()); // SELECT 1건
+
+            //Then
+            assertTrue(findMember != null);
+            assertEquals(member, findMember);
+            assertTrue(member.getName().equals(findMember.getName()));
+            assertTrue(member.getAge() == findMember.getAge());
+
+        });
+    }
+
+    @Test
+    public void testUpdate() {
+        wrapTryCatch(() -> {
+
+            //Given
+            String id = "id";
+            String newName = "New Name";
+            int newAge = 100;
+
+            Member member = insertDummy(id, "hello", 20);
+
+            //When
+            member.changeName(newName);
+            member.changeAge(newAge);
+
+            Member findMember = entityManager.find(Member.class, id);
+
+            //Then
+            assertTrue(findMember.getName().equals(newName));
+            assertTrue(findMember.getAge() == newAge);
+        });
+    }
+
+    @Test
+    public void testSelectAll() {
+        wrapTryCatch(() -> {
+
+            //Given
+            int size = 100;
+            for (int i = 0; i < size; i++) {
+                Member member = new Member("" + i, "name" + i, i);
+                entityManager.persist(member);
+            }
+
+            //When
+            List<Member> members = entityManager.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            //Then
+            assertTrue(members.size() == size);
+        });
+    }
+
+    private Member insertDummy(String id, String name, int age) {
+        Member member = new Member(id, name, age);
+        entityManager.persist(member);
+        return entityManager.find(Member.class, id);
+    }
+
+    private interface Command {
+        void execute();
+    }
+
+    private void wrapTryCatch(Command command) {
+        try {
+            transaction.begin();
+
+            command.execute();
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+}
